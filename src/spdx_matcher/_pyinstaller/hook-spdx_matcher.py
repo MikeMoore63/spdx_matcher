6a475de52a6dd5b06626f0a34511ff32f14a1916
Mike Moore <z_z_zebra@yahoo.com>
2023-03-18 10:27:32 +0000

    fix issues with hooks

--- src/spdx_matcher/_pyinstaller/hook-spdx_matcher.py
+++ src/spdx_matcher/_pyinstaller/hook-spdx_matcher.py
@@ -1,3 +1,3 @@
 from PyInstaller.utils.hooks import collect_data_files
 
-datas = collect_data_files('spdx_matcher', excludes=['__pyinstaller'])
+datas = collect_data_files('spdx_matcher', excludes=['_pyinstaller'])
